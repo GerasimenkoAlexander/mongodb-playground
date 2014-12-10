ca2d2702b1790b22a169cdb1cdba6e52145b9510
Gerasimneko <ale.gerasimenko@nixsolutins.com>
2014-12-10 19:23:13 +0200

    Add script and css to layout

--- app/layout.php
+++ app/layout.php
@@ -2,7 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mongo Playground</title>
+    <link href="/components/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <script type="text/javascript" src="/components/jquery/dist/jquery.js"></script>
+    <script type="text/javascript" src="/components/bootstrap/dist/js/bootstrap.js"></script>
+    <script type="text/javascript" src="/components/angular/angular.js"></script>
+    <script type="text/javascript" src="/components/angular-route/angular-route.js"></script>
+    <script type="text/javascript" src="/components/angular-resource/angular-resource.js"></script>
+    <script type="text/javascript" src="/components/angular-contenteditable/angular-contenteditable.js"></script>
 </head>
 <body>
     Libs and git init/ ignore and config sample
