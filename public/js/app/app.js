dd46958e355da5ad7d026fccf54c401e4bbe8ed8
Gerasimneko <ale.gerasimenko@nixsolutins.com>
2014-12-12 16:14:40 +0200

    Make angular controllers

---
+++ public/js/app/app.js
@@ -0,0 +1,22 @@
+var mongoPlayground = angular.module('mongodb-playground', []);
+
+mongoPlayground.controller('mainCtrl', ['$scope', function($scope){
+
+}]);
+
+mongoPlayground.controller('examplesCtrl', ['$scope', function($scope){
+
+}]);
+
+mongoPlayground.controller('descriptionCtrl', ['$scope', function($scope){
+
+}]);
+
+mongoPlayground.controller('consoleCtrl', ['$scope', function($scope){
+
+}]);
+
+mongoPlayground.controller('outputCtrl', ['$scope', function($scope){
+
+}]);
+
